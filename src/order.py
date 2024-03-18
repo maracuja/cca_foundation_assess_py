5a38faf8ee3a0302c1acd0c042585bf538e3fe27
David George <david.hamilton@kayenta.io>
2024-03-18 14:19:58 +0000

    calculate total with shipping, added first tests for legacy code

--- src/order.py
+++ src/order.py
@@ -2,12 +2,16 @@ from dataclasses import dataclass
 
 from src.address import Address
 from src.product import Product
+from src.shipping import calculate_shipping
 
 
 @dataclass
 class Item:
     product: Product
     quantity: int
+
+    def get_subtotal(self):
+        return self.product.price * self.quantity
 
 
 @dataclass
@@ -17,3 +21,8 @@ class Order:
 
     def add_item(self, item: Item):
         self.items.append(item)
+
+    def get_total(self):
+        total = sum(item.get_subtotal() for item in self.items)
+        shipping = calculate_shipping(self.shipping_address.country.value, total)
+        return total + shipping
--- test/test_order.py
+++ test/test_order.py
@@ -1,4 +1,5 @@
-import unittest
+from dataclasses import dataclass
+import pytest
 
 from src.order import Item, Order
 from src.product import Product
@@ -6,28 +7,121 @@ from src.address import Address
 from src.countries import Country
 
 
-class OrderTest(unittest.TestCase):
-    def test_initialise_order(self):
-        address = Address(
-            house="45",
-            street="My Street",
-            city="London",
-            postcode="AB3 4EF",
-            country=Country.UNITED_KINGDOM,
-        )
-        order = Order(shipping_address=address, items=[])
-        assert len(order.items) == 0
+def test_initialise_order():
+    address = Address(
+        house="45",
+        street="My Street",
+        city="London",
+        postcode="AB3 4EF",
+        country=Country.UNITED_KINGDOM,
+    )
+    order = Order(shipping_address=address, items=[])
+    assert len(order.items) == 0
 
-    def test_add_item_to_order(self):
-        product = Product(id=1, description="Celtic Jersey", price=49.99)
-        item = Item(product=product, quantity=1)
-        address = Address(
-            house="45",
-            street="My Street",
-            city="London",
-            postcode="AB3 4EF",
+
+def test_add_item_to_order():
+    product = Product(id=1, description="Celtic Jersey", price=49.99)
+    item = Item(product=product, quantity=1)
+    address = Address(
+        house="45",
+        street="My Street",
+        city="London",
+        postcode="AB3 4EF",
+        country=Country.UNITED_KINGDOM,
+    )
+    order = Order(shipping_address=address, items=[])
+    order.add_item(item)
+    assert len(order.items) == 1
+
+
+@pytest.mark.parametrize(
+    "price, quantity, expected_total",
+    [
+        (
+            10.00,
+            1,
+            10.00,
+        ),
+        (
+            7.50,
+            3,
+            22.50,
+        ),
+    ],
+)
+def test_subtotals(price, quantity, expected_total):
+    product = Product(id=1, description="Goalie Gloves", price=price)
+    item = Item(product=product, quantity=quantity)
+    assert item.get_subtotal() == expected_total
+
+
+@dataclass
+class OrderTotalCase:
+    products: list[Product]
+    country: Country
+    expected_total: float
+
+
+@pytest.mark.parametrize(
+    "order_total_case",
+    [
+        OrderTotalCase(
+            products=[
+                Product(id=1, description="Celtic Jersey", price=51.00),
+                Product(id=2, description="Partick Jersey", price=51.00),
+            ],
             country=Country.UNITED_KINGDOM,
-        )
-        order = Order(shipping_address=address, items=[])
-        order.add_item(item)
-        assert len(order.items) == 1
+            expected_total=102.00,
+        ),
+        OrderTotalCase(
+            products=[
+                Product(id=1, description="Celtic Jersey", price=51.00),
+                Product(id=2, description="Partick Jersey", price=51.00),
+            ],
+            country=Country.FRANCE,
+            expected_total=106.99,
+        ),
+        OrderTotalCase(
+            products=[
+                Product(id=1, description="Celtic Jersey", price=51.00),
+                Product(id=2, description="Partick Jersey", price=51.00),
+            ],
+            country=Country.ARMENIA,
+            expected_total=111.99,
+        ),
+        OrderTotalCase(
+            products=[
+                Product(id=1, description="Hibs Jersey", price=45.00),
+            ],
+            country=Country.UNITED_KINGDOM,
+            expected_total=49.99,
+        ),
+        OrderTotalCase(
+            products=[
+                Product(id=1, description="Hibs Jersey", price=45.00),
+            ],
+            country=Country.FRANCE,
+            expected_total=53.99,
+        ),
+        OrderTotalCase(
+            products=[
+                Product(id=1, description="Hibs Jersey", price=45.00),
+            ],
+            country=Country.ARMENIA,
+            expected_total=54.99,
+        ),
+    ],
+)
+def test_order_total_with_shipping(order_total_case):
+    address = Address(
+        house="45",
+        street="My Street",
+        city="London",
+        postcode="AB3 4EF",
+        country=order_total_case.country,
+    )
+    order = Order(shipping_address=address, items=[])
+    for product in order_total_case.products:
+        order.add_item(Item(product=product, quantity=1))
+
+    assert order.get_total() == order_total_case.expected_total
