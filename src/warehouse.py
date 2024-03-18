63931a546600cee34eb3340aacd3736ccaac5a75
David George <david.hamilton@kayenta.io>
2024-03-18 14:56:21 +0000

    refactored warehouse object

--- src/warehouse.py
+++ src/warehouse.py
@@ -6,6 +6,10 @@ class InsufficientStockError(Exception):
     pass
 
 
+class ProductNotFoundError(Exception):
+    pass
+
+
 @dataclass
 class Entry:
     product: Product
@@ -14,23 +18,32 @@ class Entry:
 
 @dataclass
 class Warehouse:
-    catalogue: list[Entry]
+    catalogue: dict[int, Entry]
+
+    def add_entry(self, entry: Entry):
+        if entry.product.id in self.catalogue:
+            self.receive_stock(entry.product, entry.stock)
+        else:
+            self.catalogue[entry.product.id] = entry
+
+    def get_entry(self, product: Product):
+        entry = self.catalogue.get(product.id)
+        if not entry:
+            raise ProductNotFoundError(
+                f"There is no {product.description} in the warehouse."
+            )
+        return entry
 
     def check_stock(self, product: Product) -> int:
-        for entry in self.catalogue:
-            if entry.product == product:
-                return entry.stock
+        return self.get_entry(product).stock
 
     def adjust_stock(self, product: Product, quantity: int):
-        for entry in self.catalogue:
-            if entry.product == product:
-                if quantity > entry.stock:
-                    raise InsufficientStockError(
-                        f"Trying to deduct {quantity}, but only {entry.stock} in stock."
-                    )
-                entry.stock -= quantity
+        entry = self.get_entry(product)
+        if quantity > entry.stock:
+            raise InsufficientStockError(
+                f"Trying to deduct {quantity}, but only {entry.stock} in stock."
+            )
+        entry.stock -= quantity
 
     def receive_stock(self, product: Product, quantity: int):
-        for entry in self.catalogue:
-            if entry.product == product:
-                entry.stock += quantity
+        self.get_entry(product).stock += quantity
--- test/test_warehouse.py
+++ test/test_warehouse.py
@@ -2,14 +2,14 @@ from dataclasses import dataclass
 import pytest
 
 from src.product import Product
-from src.warehouse import Entry, Warehouse, InsufficientStockError
+from src.warehouse import Entry, Warehouse, InsufficientStockError, ProductNotFoundError
 
 
 @pytest.mark.parametrize("stock", [0, 1, 2, 3, 4, 10])
 def test_check_stock(stock):
     product = Product(id=1, description="Celtic Jersey", price=50.00)
     entry = Entry(product=product, stock=stock)
-    warehouse = Warehouse(catalogue=[entry])
+    warehouse = Warehouse(catalogue={1: entry})
     assert warehouse.check_stock(product) == stock
 
 
@@ -30,7 +30,7 @@ class StockCheckCase:
 def test_adjust_stock(stock_check_case):
     product = Product(id=1, description="Celtic Jersey", price=50.00)
     entry = Entry(product=product, stock=stock_check_case.initial)
-    warehouse = Warehouse(catalogue=[entry])
+    warehouse = Warehouse(catalogue={1: entry})
     warehouse.adjust_stock(product, stock_check_case.adjustment)
     assert warehouse.check_stock(product) == stock_check_case.expected
 
@@ -38,7 +38,7 @@ def test_adjust_stock(stock_check_case):
 def test_not_enough_stock():
     product = Product(id=1, description="Celtic Jersey", price=50.00)
     entry = Entry(product=product, stock=10)
-    warehouse = Warehouse(catalogue=[entry])
+    warehouse = Warehouse(catalogue={1: entry})
     with pytest.raises(InsufficientStockError):
         warehouse.adjust_stock(product, 11)
 
@@ -54,6 +54,22 @@ def test_not_enough_stock():
 def test_receive_stock(stock_check_case):
     product = Product(id=1, description="Celtic Jersey", price=50.00)
     entry = Entry(product=product, stock=stock_check_case.initial)
-    warehouse = Warehouse(catalogue=[entry])
+    warehouse = Warehouse(catalogue={1: entry})
     warehouse.receive_stock(product, stock_check_case.adjustment)
     assert warehouse.check_stock(product) == stock_check_case.expected
+
+
+def test_add_already_in_warehouse_product():
+    product = Product(id=1, description="Celtic Jersey", price=50.00)
+    entry = Entry(product=product, stock=1)
+    warehouse = Warehouse(catalogue={1: entry})
+    warehouse.add_entry(entry)
+    assert warehouse.check_stock(product) == 2
+
+
+def test_product_not_found():
+    product = Product(id=1, description="Celtic Jersey", price=50.00)
+    entry = Entry(product=product, stock=1)
+    warehouse = Warehouse(catalogue={1: entry})
+    with pytest.raises(ProductNotFoundError):
+        warehouse.get_entry(Product(id=2, description="Rangers Trophy", price=10.00))
